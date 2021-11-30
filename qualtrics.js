3884e1de8cadb9b7f45244672b384f4fbf1c9642
Tim Ritchey <tritchey@redromelogic.com>
2021-11-29 23:10:34 -0500

    push latest changes

---
+++ qualtrics.js
@@ -0,0 +1,79 @@
+Qualtrics.SurveyEngine.addOnload(function () {
+
+	/*Place your JavaScript here to run when the page loads*/
+	
+	/* Change 1: Hiding the Next button */
+	// Retrieve Qualtrics object and save in qthis
+	var qthis = this;
+	
+	// Hide buttons
+	qthis.hideNextButton();
+	
+	/* Change 2: Defining and load required resources */
+	var task_github = "https://tritchey.github.io/reading-span/"; // https://<your-github-username>.github.io/<your-experiment-name>
+	
+	// requiredResources must include all the JS files that demo-simple-rt-task-transformed.html uses.
+	var requiredResources = [
+		task_github + "jspsych-6.0.2/plugins/jspsych-audio-keyboard-response.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-audio-button-response.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-html-keyboard-response.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-image-keyboard-response.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-html-button-response.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-fullscreen.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-external-html.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-survey-text.js",
+		task_github + "jspsych-6.0.2/plugins/jspsych-survey-likert.js",
+		task_github + "rspan.js",
+		task_github + "reading-span-main.js"
+	];
+	
+	function loadScript(idx) {
+		console.log("Loading ", requiredResources[idx]);
+		jQuery.getScript(requiredResources[idx], function () {
+			if ((idx + 1) < requiredResources.length) {
+				loadScript(idx + 1);
+			} else {
+				initExp();
+			}
+		});
+	}
+
+	if (window.Qualtrics && (!window.frameElement || window.frameElement.id !== "mobile-preview-view")) {
+		loadScript(0);
+	}
+	
+	/* Change 3: Appending the display_stage Div using jQuery */
+	// jQuery is loaded in Qualtrics by default
+	jQuery("<div id = 'display_stage_background'></div>").appendTo('body');
+	jQuery("<div id = 'display_stage'></div>").appendTo('body');
+
+
+	/* Change 4: Wrapping jsPsych.init() in a function */
+	function initExp() {
+	
+		jsPsych.init({
+			timeline: timeline,
+			display_element: 'display_stage',
+	
+			/* Change 5: Adding the clean up and continue functions.*/
+			on_finish: function (data) {
+				// clear the stage
+				jQuery('display_stage').remove();
+				jQuery('display_stage_background').remove();
+	
+				// simulate click on Qualtrics "next" button, making use of the Qualtrics JS API
+				qthis.clickNextButton();
+			}
+		});
+	}
+});
+
+Qualtrics.SurveyEngine.addOnReady(function () {
+/*Place your JavaScript here to run when the page is fully displayed*/
+
+});
+
+Qualtrics.SurveyEngine.addOnUnload(function () {
+/*Place your JavaScript here to run when the page is unloaded*/
+
+});
